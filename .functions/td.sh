c29b593a61529a37f7e4c955886411ff4c49a0fb
Brandon Morse <brandon@morsecodemedia.com>
2026-06-03 16:13:52 -0400

    updates the td functionality

--- .functions/td.sh
+++ .functions/td.sh
@@ -4,5 +4,9 @@ td() {
   local todo_dir
   todo_dir="$(dirname "$TODO")"
 
-  tmux split-window -v -p 25 "cd '$todo_dir' && todo && exec ${SHELL} -l"
+  tmux split-window -v -p 30 \
+    "export TODOFILE_OVERRIDE='$TODO'; \
+    cd '$todo_dir'; \
+    todo; \
+    exec ${SHELL} -l"
 }
