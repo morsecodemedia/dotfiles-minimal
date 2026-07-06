f84eb0b5713db3dcc1ca9cb845c209c29b85fb0c
Brandon Morse <brandon@morsecodemedia.com>
2026-07-06 19:06:36 -0400

    feat(knowledge): introduce workstation conventions

---
+++ knowledge/workstation.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+WORKSTATION_REQUIRED_FILES=(
+    "README.md"
+    ".gitignore"
+)
+
+WORKSTATION_OPTIONAL_FILES=(
+    "PLAN.md"
+    "ROADMAP.md"
+    "ARCHITECTURE.md"
+    "AI_SUBSYSTEM.md"
+)
